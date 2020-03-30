99019917623823ea2876c65a4d2ea9ebca6e2809
Parham Alvani <parham.alvani@gmail.com>
2020-03-30 22:34:56 +0430

    feat: Add Messages Type

---
+++ message/message.go
@@ -0,0 +1,10 @@
+package message
+
+const (
+	// New creates new room for game
+	New = "/newgame"
+	// Join joins the sender to the room
+	Join = "/join"
+	// Start tarts a new game in the room
+	Start = "/startgame"
+)
