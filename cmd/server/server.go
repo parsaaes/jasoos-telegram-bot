6767b43d8341c244263e3407c7e15531dcdcd5c8
Parham Alvani <parham.alvani@gmail.com>
2020-04-01 13:52:49 +0430

    feat: Pass Words to Engine

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -13,7 +13,7 @@ func Cmd(cfg config.Config) *cobra.Command {
 		Use:   "server",
 		Short: "start the game server",
 		Run: func(cmd *cobra.Command, args []string) {
-			eng, err := engine.New(cfg.Token)
+			eng, err := engine.New(cfg.Token, cfg.Words)
 			if err != nil {
 				logrus.Fatalf("server: cannot create engine: %s", err.Error())
 				return
