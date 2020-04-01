1ac2d52c3f1e66900902656fff3eb51bca18590b
Parham Alvani <parham.alvani@gmail.com>
2020-04-01 04:52:44 +0000

    feat: Handle Room State

--- room/room.go
+++ room/room.go
@@ -2,16 +2,22 @@ package room
 
 import (
 	"fmt"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/parsaaes/jasoos-telegram-bot/message"
 )
 
+// JoinDuration in seconds
+const JoinDuration = 30
+
+// Member represents a member of a game room
 type Member struct {
 	Name string
 	ID   int
 }
 
+// Room is a place for playing
 type Room struct {
 	ChatID   int64
 	State    State
@@ -36,9 +42,41 @@ func (r *Room) Created() {
 	})
 
 	r.SendChan <- msg
+
+	go r.CountToStart()
+	r.State = Join
 }
 
+// CountToStart conts 30 seconds then start the game
+func (r *Room) CountToStart() {
+	tick := time.NewTicker(1 * time.Second)
+	count := 0
+
+	for range tick.C {
+		if count%10 == 0 {
+			r.SendChan <- tgbotapi.NewMessage(
+				r.ChatID,
+				fmt.Sprintf("%d sec left to join", JoinDuration-count),
+			)
+		}
+		count++
+		if count == JoinDuration {
+			tick.Stop()
+		}
+	}
+
+	r.SendChan <- tgbotapi.NewMessage(
+		r.ChatID,
+		"Let's play",
+	)
+}
+
+// Joined must be called when a new member joined
 func (r *Room) Joined(from *tgbotapi.User, base *tgbotapi.Message) {
+	if r.State != Join {
+		return
+	}
+
 	if from == nil || base == nil {
 		return
 	}
